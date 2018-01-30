7f75c90c664ef77e4c564bbb434893b81c334b58
KevinReaves <kevin82593@hotmail.com>
2018-01-29 18:27:18 -0600

    50 - pow(x, n)

---
+++ 50 - pow(x, n).py
@@ -0,0 +1,28 @@
+"""
+Implement pow(x, n).
+
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+"""
+
+class Solution:
+    def myPow(self, x, n):
+        #return x ** n
+        if not n:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n % 2:
+            return x * self.myPow(x, n-1)
+        return self.myPow(x*x, n/2)
+
+solution = Solution()
+
+print(solution.myPow(10, 50))
